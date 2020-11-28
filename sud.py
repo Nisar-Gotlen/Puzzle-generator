576e5bf5244f0488d7401f99b72a6f5690bc790d
chlus000 <krislyt@mail.ru>
2020-11-28 20:06:44 +1000

    Added visualization of the seedcode

--- sud.py
+++ sud.py
@@ -1,10 +1,13 @@
+import random
 from tkinter import *
-from solvingMethods import puzzle, puzzleSolving
+from tkinter import messagebox
+from pole1 import puzzle, puzzleSolved
+import generation
 
 pole=int(9)
 size=50
 
-def print_board(board):
+def printBoard(board):
     global canvas
     x=5
     canvas = Canvas(window, width=pole*size+x*2-3,height=pole*size+x*2-3,bg="white")
@@ -28,24 +31,56 @@ def print_board(board):
                 canvas.create_line(x2,y2,x2,y1,width=3)
     canvas.pack(side=LEFT, padx=50,pady=20)
 
-def click_button():
+def clickBtn():
     btn.place_forget()
     canvas.pack_forget()
-    boardSolved=puzzleSolving
-    print_board(boardSolved)
+    boardSolved=puzzleSolved
+    printBoard(boardSolved)
+
+def seedBtn():
+    global seedMes
+    seedMes=message.get()
+    if seedMes.isdigit():
+        if int(seedMes)<generation.num:
+            canvas.pack_forget()
+            random.seed(seedMes)
+            firstBoard=generation.baseBoard()
+            board=generation.mixing(firstBoard, int(seedMes))
+            printBoard(board)
+            mesEntry.delete("0", "end")
+            seedText.config(text=seedMes)
+        else:
+             messagebox.showinfo("Error","Seed in the wrong range")
+    else:
+        messagebox.showinfo("Error", "Wrong seed")
 
 
 
 window=Tk()
 window.title("SUDOKU")
 window.geometry("800x550")
-board=puzzle
-print_board(board)
+board,genSeed=generation.main()
+printBoard(board)
 
-btn = Button(window, text="Solve",background="#235",foreground="white",font="Arial 16", width=15, command=click_button)
+btn = Button(window, text="Solve",background="#235",foreground="white",font="Arial 16", width=15, command=clickBtn)
 btn.place(x=550,y=50)
 
 btnExit=Button(window,text="Exit",background="#235",foreground="white",font="Arial 16",width=15,command=window.destroy)
 btnExit.place(x=550,y=100)
 
+textLabel=Label(text="Your seed:", foreground="black",font="Arial 20",width=15, justify="center")
+textLabel.place(x=525,y=200)
+
+seedText=Label(text=genSeed, foreground="black",font="Arial 20",width=15, justify="center")
+seedText.place(x=525,y=250)
+
+message = StringVar()
+mesEntry = Entry(textvariable=message, background="white",foreground="black",font="Arial 16",width=15, justify="center", bd="3",)
+mesEntry.insert (0, "Enter seed")
+mesEntry.place (x=550,y=400)
+
+btnOk=Button(window,text="Enter",background="#235",foreground="white",font="Arial 16",width=15, command=seedBtn)
+btnOk.place(x=550,y=450)
+
 window.mainloop()
+
