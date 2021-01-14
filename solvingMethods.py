d483504d5aedabe3234abdde6c88964ab1142f80
Саша <Sandra.lapokha@gmail.com>
2021-01-14 18:21:40 +1000

    add new methid named hiddenTriple`

--- solvingMethods.py
+++ solvingMethods.py
@@ -406,3 +406,22 @@ class SolvingMethods(object):
                                         self.deleteExtraValues()
                                         self.changes = True
                                         self.isHidPair = True
+
+
+puzzle = [
+    [4, 0, 0, 0, 0, 0, 9, 3, 8],
+    [0, 3, 2, 0, 9, 4, 1, 0, 0],
+    [0, 9, 5, 3, 0, 0, 2, 4, 0],
+    [3, 7, 0, 6, 0, 9, 0, 0, 4],
+    [5, 2, 9, 0, 0, 1, 6, 7, 3],
+    [6, 0, 4, 7, 0, 3, 0, 9, 0],
+    [9, 5, 7, 0, 0, 8, 3, 0, 0],
+    [0, 0, 3, 9, 0, 0, 4, 0, 0],
+    [2, 4, 0, 0, 3, 0, 7, 0, 9]
+]
+
+solv = SolvingMethods(puzzle)
+for i in range(9):
+    print(solv.numberField[i])
+
+print(solv.isPossible())
